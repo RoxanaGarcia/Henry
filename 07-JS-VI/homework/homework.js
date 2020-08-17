a79e38b6bc0721ce07aed3cc5bb2ac90875bcd4c
Ley <Leyra627@gmail.com>
2020-08-16 21:35:03 -0300

    Homework Completos

---
+++ 07-JS-VI/homework/homework.js
@@ -0,0 +1,68 @@
+// Do not change any of the function names
+
+function invocarCallback(cb) {
+  // Invoca al callback `cb`
+  cb();
+}
+
+function sumarArray(numeros, cb) {
+  // Suma todos los números enteros (int/integers) de un array ("numeros")
+  // Pasa el resultado a `cb`
+  // No es necesario devolver nada
+ // var numeros = [11,22,32,45,54]
+  // var suma = 0;
+  // for(var i = 0; i < numeros.length; i++) {
+  //   suma = suma + numeros[i];
+  // }
+  // cb(suma);
+
+  // Segunda opcion
+  var sumaTotal = numeros.reduce(function(acc, curr) {
+    return acc + curr;
+  },0);
+  cb(sumaTotal);
+}
+
+function forEach(array, cb) {
+  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+  // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+ // array.forEach(function(elemento, index) {
+  //   cb(elemento);
+  // });
+  /*for(var i = 0; i < array.length; i++) {
+    cb(array[i]);*/
+    
+  array.forEach(function(elemento){
+      cb(elemento);
+    })
+    //array.forEach(cb(elemento))
+  }
+
+function map(array, cb) {
+    // Crea un nuevo array
+  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+  // El nuevo array debe tener la misma longitud que el array del argumento
+  //map devuelve un nuevo array modificado
+  // var array = [1,2,3,4,5]
+  // function cb (e) {console.log(e)}
+  // nuevoarray = []
+  // var nuevoArray = [];
+  // for(var i = 0; i < array.length; i++) {
+  //   nuevoArray.push(cb(array[i]));
+  //   nuevoArray[i] = cb(array[i]);
+  // }
+  var nuevoArray = array.map(function(elemento) {
+    return cb(elemento);
+  });
+  return nuevoArray;
+}
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  invocarCallback,
+  sumarArray,
+  forEach,
+  map,
+};
